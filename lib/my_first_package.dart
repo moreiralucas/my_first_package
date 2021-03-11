75dadf218f63f9ee5ad25f1c59613dedb994e806
Lucas Moreira Pires <lucas-mopi@hotmail.com>
2021-03-10 22:49:50 -0300

    initial commit

---
+++ lib/my_first_package.dart
@@ -0,0 +1,6 @@
+library my_first_package;
+
+/// A Calculator.
+export 'package:flutter_modular/flutter_modular.dart';
+
+export 'src/calculator.dart';
---
+++ lib/src/calculator.dart
@@ -0,0 +1,10 @@
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  /// Returns [value] minus 1.
+  int minusOne(int value) => value - 1;
+
+  /// Returns square root of [value].
+  int squareRoot(int value) => value * value;
+}
