f27d8f9663bc87fb2fa6c6be9d8927a7d6224dcf
Lucas Moreira Pires <lucas-mopi@hotmail.com>
2021-03-11 09:30:31 -0300

    Add flutter modular into pubspec

--- lib/my_first_package.dart
+++ lib/my_first_package.dart
@@ -4,3 +4,5 @@ library my_first_package;
 export 'package:flutter_modular/flutter_modular.dart';
 
 export 'src/calculator.dart';
+export 'src/my_app.dart' show MyCustomApp;
+export 'src/app_module.dart' show CustomAppModule;
